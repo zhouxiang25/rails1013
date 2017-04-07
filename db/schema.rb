481f6de3b5411038415f276ab2905aa45e26cbcd
周祥 <xingzhewujiangcz@gmail.com>
2017-04-08 00:49:14 +0800

    show group's creator info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407154249) do
+ActiveRecord::Schema.define(version: 20170407155513) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
