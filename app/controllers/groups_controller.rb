dc42eae208c13c74dd827648d92b568253817408
周祥 <xingzhewujiangcz@gmail.com>
2017-04-10 07:31:05 +0800

    join&quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy, :join, :quit]
   def index
     @groups = Group.all
   end
@@ -8,7 +8,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
-  
+
   def edit
 
   end
@@ -50,6 +50,31 @@ class GroupsController < ApplicationController
  end
 
 
+
+ def join
+   @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+       current_user.join!(@group)
+       flash[:notice] = "加入本讨论版成功！"
+    else
+      flash[:warning] = "你已经是本讨论版成员了！"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！"
+        else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+
+   redirect_to group_path(@group)
+  end
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
+
+    member do
+      post :join
+      post :quit
+    end
+
+
     resources :posts
+
   end
   root 'groups#index'
 
